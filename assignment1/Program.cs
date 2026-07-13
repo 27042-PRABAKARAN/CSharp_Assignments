312a44686e9185dbe378f3b8f8577066476459f1
Prabakaran  Selvam <sb.prabakaran@solitontech.com>
2026-07-13 17:54:25 +0530

    feat:created methods for adding viewing and deleting contact

--- assignment1/Program.cs
+++ assignment1/Program.cs
@@ -25,53 +25,69 @@ namespace Assignments
             void DeleteContact()
             {
                 Console.WriteLine("Deleting a contact");
+                ViewContact();
+                Console.WriteLine("Enter the Sno:");
+                string deleteContactindex = Console.ReadLine();
+                int index = int.Parse(deleteContactindex);
+                masterList.RemoveAt(index - 1);
                 Console.WriteLine("Deleted Succesfully");
             }
             void EditContact()
-        {
-            Console.WriteLine("Editing contact");
-            Console.WriteLine("Edited Succesfully");
+            {
+                Console.WriteLine("Editing contact");
+                Console.WriteLine("Edited Succesfully");
             }
 
-            static void SearchContact()
-        {
-            Console.WriteLine("Searching Contact");
-        }
+            void SearchContact()
+            {
+                    Console.WriteLine("Searching Contact");
+                }
 
             void ViewContact()
-        {
-            Console.WriteLine("viewing list of contacts");
-        }
+            {
+                Console.WriteLine("viewing list of contacts");
+                int i = 0;
+                foreach (var entry in masterList)
+                {
+                    Console.Write(i + 1 + ".");
+                    foreach (var item in entry)
+                    {
+                        Console.Write($"{item} ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("end of list");
+            }
             void AddContact()
-        {
-            List<string> newContact = new List<string>();
-            Console.WriteLine("Adding a contact");
-            Console.WriteLine("Enter name : ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter dob as (dd/mm/yyyy) : ");
-            string dob = Console.ReadLine();
-            Console.WriteLine("Enter phone : ");
-            string phone = Console.ReadLine();
-            Console.WriteLine("Enter other details : ");
-            string otherDetails = Console.ReadLine();
-            newContact.Add(name);
-            newContact.Add(dob);
-            newContact.Add(phone);
-            newContact.Add(otherDetails);
-            masterList.Add(newContact);
-            Console.WriteLine("Added succesfully");
-        }
+            {
+                List<string> newContact = new List<string>();
+                Console.WriteLine("Adding a contact");
+                Console.WriteLine("Enter name : ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter dob as (dd/mm/yyyy) : ");
+                string dob = Console.ReadLine();
+                Console.WriteLine("Enter phone : ");
+                string phone = Console.ReadLine();
+                Console.WriteLine("Enter other details : ");
+                string otherDetails = Console.ReadLine();
+                newContact.Add(name);
+                newContact.Add(dob);
+                newContact.Add(phone);
+                newContact.Add(otherDetails);
+                masterList.Add(newContact);
+                Console.WriteLine("Added succesfully");
+            }
 
             static void PrintOperationsAvailable()
-        {
-            Console.WriteLine(
-                "1. [A]dd a contact.\n" +
-                "2. [V]iew all contacts.\n" +
-                "3. [S]earch a contact.\n" +
-                "4. [E]dit a contact.\n" +
-                "5. [D]elete a contact.\n"+
-                "5. type exit to exit the app.");
+            {
+                Console.WriteLine(
+                    "1. [A]dd a contact.\n" +
+                    "2. [V]iew all contacts.\n" +
+                    "3. [S]earch a contact.\n" +
+                    "4. [E]dit a contact.\n" +
+                    "5. [D]elete a contact.\n" +
+                    "5. type exit to exit the app.");
+            }
         }
     }
-  }
 }
