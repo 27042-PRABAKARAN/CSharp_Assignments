dacbe43222b81d5f73045cfb833fc0958c3758ac
Prabakaran  Selvam <sb.prabakaran@solitontech.com>
2026-07-13 18:45:46 +0530

    feat:search edit and delete methods implemented

--- assignment1/Program.cs
+++ assignment1/Program.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 namespace Assignments
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            List<List<string>> masterList = new List<List<string>>();
+            List<List<string>> masterList = new List<List<string>>(); 
+            //the master list that is where all the data is stored and manipulated.
             bool endApp = false;
             Console.WriteLine("Welcome to contact manager");
             while (!endApp)
@@ -20,64 +22,9 @@ namespace Assignments
                     case "e": EditContact(); break;
                     case "d": DeleteContact(); break;
                     case "exit": endApp = true; break;
+                    default: Console.WriteLine("Enter a valid choice");break;
                 }
             }
-            void DeleteContact()
-            {
-                Console.WriteLine("Deleting a contact");
-                ViewContact();
-                Console.WriteLine("Enter the Sno:");
-                string deleteContactindex = Console.ReadLine();
-                int index = int.Parse(deleteContactindex);
-                masterList.RemoveAt(index - 1);
-                Console.WriteLine("Deleted Succesfully");
-            }
-            void EditContact()
-            {
-                Console.WriteLine("Editing contact");
-                Console.WriteLine("Edited Succesfully");
-            }
-
-            void SearchContact()
-            {
-                    Console.WriteLine("Searching Contact");
-                }
-
-            void ViewContact()
-            {
-                Console.WriteLine("viewing list of contacts");
-                int i = 0;
-                foreach (var entry in masterList)
-                {
-                    Console.Write(i + 1 + ".");
-                    foreach (var item in entry)
-                    {
-                        Console.Write($"{item} ");
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine("end of list");
-            }
-            void AddContact()
-            {
-                List<string> newContact = new List<string>();
-                Console.WriteLine("Adding a contact");
-                Console.WriteLine("Enter name : ");
-                string name = Console.ReadLine();
-                Console.WriteLine("Enter dob as (dd/mm/yyyy) : ");
-                string dob = Console.ReadLine();
-                Console.WriteLine("Enter phone : ");
-                string phone = Console.ReadLine();
-                Console.WriteLine("Enter other details : ");
-                string otherDetails = Console.ReadLine();
-                newContact.Add(name);
-                newContact.Add(dob);
-                newContact.Add(phone);
-                newContact.Add(otherDetails);
-                masterList.Add(newContact);
-                Console.WriteLine("Added succesfully");
-            }
-
             static void PrintOperationsAvailable()
             {
                 Console.WriteLine(
@@ -86,7 +33,166 @@ namespace Assignments
                     "3. [S]earch a contact.\n" +
                     "4. [E]dit a contact.\n" +
                     "5. [D]elete a contact.\n" +
-                    "5. type exit to exit the app.");
+                    "6. type exit to exit the app.");
+            }
+            void AddContact()
+            { // adding a new contact
+                List<string> newContact = new List<string>();
+                Console.WriteLine("Adding a contact");
+                Console.WriteLine("Enter name : ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter email: ");
+                string email = Console.ReadLine();
+                Console.WriteLine("Enter phone : ");
+                string phone = Console.ReadLine();
+                Console.WriteLine("Enter other details : ");
+                string otherDetails = Console.ReadLine();
+                newContact.Add(name);
+                newContact.Add(email);
+                newContact.Add(phone);
+                newContact.Add(otherDetails);
+                masterList.Add(newContact);
+                Console.WriteLine("Added succesfully");
+            }
+            void ViewContact()
+            { // displaying list of contact
+                Console.WriteLine("viewing list of contacts");
+                int i = 0;
+                foreach (var entry in masterList)
+                {
+                    Console.Write((++i) + ".");
+                    foreach (var item in entry)
+                    {
+                        Console.Write($"{item} ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("end of list");
+            }
+            void SearchContact()
+            { // Searching is implemented and can be searched using various parameters such as name contact  and email
+                Console.WriteLine("Searching Contact");
+                Console.WriteLine("Search using\n [N]ame \n [C]ontact \n [E]mail");
+                string choice = Console.ReadLine();
+                switch (choice.ToLower())
+                {
+                    case "n":
+                        {
+                            bool found = false;
+                            Console.WriteLine("Enter name : ");
+                            string userchoice = Console.ReadLine();
+                            foreach (var entry in masterList)
+                            {
+                                if (entry[0] == userchoice)
+                                {
+                                    found = true;
+                                    foreach (var item in entry)
+                                    {
+                                        Console.Write($"{item} ");
+                                    }
+                                    Console.WriteLine();
+                                }
+                            }
+                            if (found == false)
+                            {
+                                Console.WriteLine("Not found");
+                            }
+
+                            break;
+                        }
+                    case "c":
+                        {
+                            bool found = false;
+                            Console.WriteLine("Enter Contact : ");
+                            string userchoice = Console.ReadLine();
+                            foreach (var entry in masterList)
+                            {
+                                if (entry[2] == userchoice)
+                                {
+                                    foreach (var item in entry)
+                                    {
+                                        found = true;
+                                        Console.Write($"{item} ");
+                                    }
+                                    Console.WriteLine();
+                                }
+                            }
+                            if (found == false)
+                            {
+                                Console.WriteLine("Not found");
+                            }
+                            break;
+                        }
+                    case "e":
+                        {
+                            bool found = false;
+                            Console.WriteLine("Enter email: ");
+                            string userchoice = Console.ReadLine();
+                            foreach (var entry in masterList)
+                            {
+                                if (entry[1] == userchoice)
+                                {
+                                    found = true;
+                                    foreach (var item in entry)
+                                    {
+                                        Console.Write($"{item} ");
+                                    }
+                                    Console.WriteLine();
+                                }
+                            }
+                            if (found == false)
+                            {
+                                Console.WriteLine("Not found");
+                            }
+                            break;
+                        }
+                    default: Console.WriteLine("Enter a valid choice"); break;
+                }
+            }
+            void EditContact()
+            {//Editing a contact using the Sno of the displyed list
+                Console.WriteLine("Editing contact");
+                ViewContact();
+                Console.WriteLine("Enter the Sno: ");
+                string editContactindex = Console.ReadLine();
+                int index;
+                if (int.TryParse(editContactindex, out index) && index >= 0 && index <= masterList.Count())
+                {
+                    Console.WriteLine("Enter name: ");
+                    String name = Console.ReadLine();
+                    Console.WriteLine("Enter Email: ");
+                    String email = Console.ReadLine();
+                    Console.WriteLine("Enter contact: ");
+                    String contact = Console.ReadLine();
+                    Console.WriteLine("Enter Other details");
+                    String otherDetails = Console.ReadLine();
+                    masterList[index - 1][0] = name;
+                    masterList[index - 1][1] = email;
+                    masterList[index - 1][2] = contact;
+                    masterList[index - 1][3] = otherDetails;
+                    Console.WriteLine("Edited Succesfully");
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid index");
+                }
+            }
+            void DeleteContact()
+            {//Deleting a contact using the Sno of displayed list
+                Console.WriteLine("Deleting a contact");
+                ViewContact();
+                Console.WriteLine("Enter the Sno:");
+                string deleteContactindex = Console.ReadLine();
+                int index;
+                if (int.TryParse(deleteContactindex, out index) && index >= 0 && index <= masterList.Count())
+                {
+                    masterList.RemoveAt(index - 1);
+                    Console.WriteLine("Deleted Succesfully");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number entered.");
+                }
             }
         }
     }
