d0df3748b1e2c531e69a6173105ada8702204635
Prabakaran  Selvam <sb.prabakaran@solitontech.com>
2026-07-20 11:17:39 +0530

    feat:started working on assignment-2

---
+++ assignment2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignments
+{
+    /// <summary>
+    /// Program class
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main function
+        /// </summary>
+        /// <param name="args"> if there are any terminal arguents</param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
