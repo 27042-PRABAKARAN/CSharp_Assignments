ca128df5a4bb59b3f70bb663f6e0a1a3f8494857
Prabakaran  Selvam <sb.prabakaran@solitontech.com>
2026-07-23 09:51:34 +0530

    feat:task3 account management system

--- assignment2/Service/BankService.cs
+++ assignment2/Service/BankService.cs
@@ -17,10 +17,10 @@ namespace Assignment2.Service
         /// </summary>
         /// <param name="capital"> the capital when account is created </param>
         /// <returns> returns the message </returns>
-        public string? CreateSavingAccount(long capital)
+        public BankAccount CreateSavingAccount(decimal? capital)
         {
             SavingsAccount newAccount = new SavingsAccount(_accounts++.ToString(), capital);
-            return "Created Succesfully";
+            return newAccount;
         }
 
         /// <summary>
@@ -28,7 +28,7 @@ namespace Assignment2.Service
         /// </summary>
         /// <param name="capital"> the capital when account is created </param>
         /// <returns> returns the message </returns>
-        public BankAccount CreateCheckingAccount(long capital)
+        public BankAccount CreateCheckingAccount(decimal? capital)
         {
             CheckingAccount newAccount = new CheckingAccount(_accounts++.ToString(), capital);
             return newAccount;
@@ -39,7 +39,7 @@ namespace Assignment2.Service
         /// </summary>
         /// <param name="account"> the account which deposit should be made </param>
         /// <param name="amount"> the amount to be deposited </param>
-        public void Deposit(BankAccount account, double amount)
+        public void Deposit(BankAccount account, decimal? amount)
         {
             account.DepositAmount(amount);
         }
@@ -50,7 +50,7 @@ namespace Assignment2.Service
         /// <param name="account"> the account which WithDraw should be made </param>
         /// <param name="amount"> the amount to be Withdrawed </param>
         /// <returns> returns the status </returns>
-        public bool WithDraw(BankAccount account, double amount)
+        public bool WithDraw(BankAccount account, decimal? amount)
         {
             if (account.Balance < amount)
             {
--- assignment2/View/BankSystem.cs
+++ assignment2/View/BankSystem.cs
@@ -1,1 +1,120 @@
-﻿
+﻿using Assignment2.Helper;
+using Assignment2.Model.Bank;
+using Assignment2.Service;
+
+namespace Assignment2.View
+{
+    /// <summary>
+    /// the View class of Employee
+    /// </summary>
+    internal class BankSystem
+    {
+        /// <summary>
+        /// instance of the _shapeService
+        /// </summary>
+        private readonly BankService _bankServices = new BankService();
+
+        /// <summary>
+        /// Operation enumerator
+        /// </summary>
+        internal enum Operation
+        {
+            /// <summary>
+            /// to create a Savings account
+            /// </summary>
+            CreateSavings = 1,
+
+            /// <summary>
+            /// to create a Checking account
+            /// </summary>
+            CreateChecking,
+
+            /// <summary>
+            /// to withdraw amount
+            /// </summary>
+            WithDraw,
+
+            /// <summary>
+            /// to exit the app
+            /// </summary>
+            Exit,
+        }
+
+        /// <summary>
+        /// Function that starts the app
+        /// </summary>
+        public void BankOperations()
+        {
+            Output.Display("Welcome to Account Management System :  ");
+            while (true)
+            {
+                Output.Display("1. Create A Saving Account.\n2. Create A Checking Account.\n3. Exit the app");
+                string? userInput = UserInput.ReadInput("Enter the choice: ");
+                int index;
+                int.TryParse(userInput, out index);
+                Operation operation = (Operation)index;
+                BankAccount? account = null;
+                switch (operation)
+                {
+                    case Operation.CreateSavings: account = this.CreateSavingAccount(); break;
+                    case Operation.CreateChecking: account = this.CreateCheckingAccount(); break;
+                    case Operation.WithDraw: this.Withdraw(account); break;
+                    case Operation.Exit: return;
+
+                    default: Output.Error("enter valid choice"); break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// creates manager
+        /// </summary>
+        /// <returns> returns the bank Account</returns>
+        public BankAccount? CreateSavingAccount()
+        {
+            decimal? amount = UserInput.ReadAmount("Enter the capital of the account: ");
+            if (amount == null)
+            {
+                return null;
+            }
+
+            return this._bankServices.CreateSavingAccount(amount);
+        }
+
+        /// <summary>
+        /// creates Developer
+        /// </summary>
+        /// <returns> returns the created bank account </returns>
+        public BankAccount? CreateCheckingAccount()
+        {
+            decimal? amount = UserInput.ReadAmount("Enter the capital of the account: ");
+            if (amount == null)
+            {
+                return null;
+            }
+
+            return this._bankServices.CreateCheckingAccount(amount);
+        }
+
+        /// <summary>
+        /// to withdraw amount
+        /// </summary>
+        /// <param name="account"> the account in which withdraw takes place</param>
+        public void Withdraw(BankAccount? account)
+        {
+            if (account == null)
+            {
+                Output.Error("No account to withdraw amount. create one first.");
+                return;
+            }
+
+            decimal? amount = UserInput.ReadAmount("Enter the amount to be deposited: ");
+            if (amount == null)
+            {
+                return;
+            }
+
+            this._bankServices.WithDraw(account, amount);
+        }
+    }
+}
