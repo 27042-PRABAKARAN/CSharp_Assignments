14cfba1cdbe1793303ef151231109faee9b17630
Prabakaran  Selvam <sb.prabakaran@solitontech.com>
2026-07-27 14:17:19 +0530

    feat:created models and repository for inventory management system

---
+++ inventoryManager/Persistence/InventoryRepository.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryManager.Models;
+
+namespace InventoryManager.Persistence
+{
+    /// <summary>
+    /// repository layer of inventory management system.
+    /// </summary>
+    internal class InventoryRepository
+    {
+        private readonly List<Product> _products = new List<Product>();
+
+        /// <summary>
+        /// to add a product
+        /// </summary>
+        /// <param name="product"> the product to be added</param>
+        public void Add(Product product)
+        {
+            this._products.Add(product);
+        }
+
+        /// <summary>
+        /// to remove a product.
+        /// </summary>
+        /// <param name="id"> the product to be removed </param>
+        /// <returns> the status of deletion </returns>
+        public bool Remove(Guid id)
+        {
+            Product? product = this.GetProduct(id);
+            if (product != null)
+            {
+                this._products.Remove(product);
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// to search by name
+        /// </summary>
+        /// <param name="name"> the name of the product to be searched </param>
+        /// <returns> list of found elements </returns>
+        public List<Product> Search(string? name)
+        {
+            return this._products.Where(e => e.Name == name).ToList();
+        }
+
+        /// <summary>
+        /// to get all the details in repository
+        /// </summary>
+        /// <returns> returns list of products available </returns>
+        public List<Product> GetAll()
+        {
+            return this._products.Select(p => new Product(
+                p.Id,
+                p.Name,
+                p.Quantity,
+                p.Price)).ToList();
+        }
+
+        /// <summary>
+        /// to udate the product details
+        /// </summary>
+        /// <param name="updateProduct"> the updated product </param>
+        /// <returns> the status of the product updation </returns>
+        public bool Update(Product updateProduct)
+        {
+            Product? product = this.GetProduct(updateProduct.Id);
+            if (product != null)
+            {
+                product.Name = updateProduct.Name;
+                product.Quantity = updateProduct.Quantity;
+                product.Price = updateProduct.Price;
+                return true;
+            }
+
+            return false;
+        }
+
+        private Product? GetProduct(Guid id)
+        {
+            return this._products.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
---
+++ inventoryManager/Service/InventoryService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using InventoryManager.Models;
+using InventoryManager.Persistence;
+
+namespace InventoryManager.Service
+{
+    /// <summary>
+    /// the service layer.
+    /// </summary>
+    internal class InventoryService
+    {
+        private readonly InventoryRepository _repository = new InventoryRepository();
+
+        /// <summary>
+        /// to create a new product.
+        /// </summary>
+        /// <param name="name"> the name of the product </param>
+        /// <param name="price"> the price of the product </param>
+        /// <param name="quantity"> the quantity of the product</param>
+        /// <returns> returns status</returns>
+        public bool Create(string? name, decimal price, decimal quantity)
+        {
+            Guid id = Guid.NewGuid();
+            Product newProduct = new Product(id, name, price, quantity);
+            this._repository.Add(newProduct);
+            return true;
+        }
+
+        /// <summary>
+        /// the delete operation takes place.
+        /// </summary>
+        /// <param name="id"> the GUID of the Product</param>
+        /// <returns> returns the status of the deletion process</returns>
+        public bool Delete(Guid id)
+        {
+            return this._repository.Remove(id);
+        }
+
+        /// <summary>
+        /// to get all the items.
+        /// </summary>
+        /// <returns> the list of products </returns>
+        public List<Product> GetAll()
+        {
+            return this._repository.GetAll();
+        }
+
+        public List<Product> Search(string? name)
+        {
+            this._repository.Search(name);
+        }
+    }
+}
