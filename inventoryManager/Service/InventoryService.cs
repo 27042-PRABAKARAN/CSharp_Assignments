0143c0920effa98836fdd38237ddd9dacf1be9c8
Prabakaran  Selvam <sb.prabakaran@solitontech.com>
2026-07-27 14:21:21 +0530

    refactor:created services

--- inventoryManager/Service/InventoryService.cs
+++ inventoryManager/Service/InventoryService.cs
@@ -51,9 +51,42 @@ namespace InventoryManager.Service
             return this._repository.GetAll();
         }
 
+        /// <summary>
+        /// the search operation
+        /// </summary>
+        /// <param name="name"> the name to be searched </param>
+        /// <returns> the list of searched product</returns>
         public List<Product> Search(string? name)
         {
-            this._repository.Search(name);
+            return this._repository.Search(name);
+        }
+
+        /// <summary>
+        /// to update
+        /// </summary>
+        /// <param name="choice"> which parameter is updated</param>
+        /// <param name="id"> on which product the upadte takes place</param>
+        /// <param name="value"> new value to be updated</param>
+        /// <returns> status of the updation</returns>
+        public bool Update(string? choice, Guid id, decimal? value)
+        {
+            Product? product = this.GetProduct(id);
+            if (product == null)
+            {
+                return false;
+            }
+
+            switch (choice)
+            {
+                case "p": product.Price = value; return this._repository.Update(product);
+                case "q": product.Quantity = value; return this._repository.Update(product);
+                default: return false;
+            }
+        }
+
+        private Product? GetProduct(Guid id)
+        {
+            return this._repository.GetAll().FirstOrDefault(p => p.Id == id);
         }
     }
 }
