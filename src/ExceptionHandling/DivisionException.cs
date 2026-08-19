10f6ffdc43eaea82d19df95fb46a3223f787d703
Prabakaran  Selvam <sb.prabakaran@solitontech.com>
2026-08-19 11:35:14 +0530

    feat: task 2 and task 3 implemented

--- src/ExceptionHandling/DivisionException.cs
+++ src/ExceptionHandling/DivisionException.cs
@@ -13,9 +13,9 @@ namespace ExceptionHandling
     internal class DivisionException
     {
         /// <summary>
-        /// Division operation
+        /// Division by zero exception
         /// </summary>
-        public void Division()
+        public void Task1()
         {
             try
             {
@@ -35,6 +35,71 @@ namespace ExceptionHandling
             finally
             {
                 Console.WriteLine("Enter any key exit division calculator");
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// Array Index Out of Bound exception
+        /// </summary>
+        public void Task2()
+        {
+            try
+            {
+                int[] array = { 1, 2, 3, 4 };
+                int index = 0;
+                while (true)
+                {
+                    Console.WriteLine(array[index++]);
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Tried to access the index out of actual array");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Enter any key exit Task 2");
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// Custom exception
+        /// </summary>
+        public void Task3()
+        {
+            try
+            {
+                Console.WriteLine("Enter 1st Number : ");
+                double? dividend = UserInput.ReadDouble("Enter dividend: ");
+                double? divisor = UserInput.ReadDouble("Enter divisor: ");
+                if (dividend == null || divisor == null)
+                {
+                    throw new UserInputException("User did not enter proper value");
+                }
+
+                Console.WriteLine($"the divided value is {dividend / divisor}");
+            }
+            catch (UserInputException)
+            {
+                Console.WriteLine("division by Zero is not possible");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("division by Zero is not possible");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid entry entered More than given tries");
+            }
+            finally
+            {
+                Console.WriteLine("Enter any key exit Task 3");
                 Console.ReadKey();
             }
         }
---
+++ src/ExceptionHandling/UserInputException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    /// <summary>
+    /// Custom Exception created
+    /// </summary>
+    internal class UserInputException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserInputException"/> class.
+        /// </summary>
+        /// <param name="message"> Exception message </param>
+        public UserInputException(string? message)
+            : base(message)
+        {
+        }
+    }
+}
