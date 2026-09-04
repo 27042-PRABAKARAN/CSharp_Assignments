4e5999040724016a5bd162d863fd89287de61bf4
PRABAKARAN S B <sb.prabakaran@solitontech.com>
2026-09-04 18:35:49 +0530

    Assignment - 8 Exceptional Handling (#8)
    * feat: Task 1 completed
    
    * feat: task 2 and task 3 implemented
    
    * feat: task 4 and 5 created
    
    * refactor: updated catch and rethrow in task 2
    
    * refactor: Used enums to choose task
    
    * refactor: task menu options to choose task
    
    * refactor: Console messages made more clear
    
    * refactor: Peer review comment resolved
    
    * refactor: Naming updated more maningfully
    
    * refactor: Enums name updated and user console improvised
    
    * refactor: generic helpers used
    
    * refactor: updated stack trace on task 5
    
    * feat: ReadMe added
    
    * refactor: updated location

---
+++ src/ExceptionHandling/Enums/TaskList.cs
@@ -0,0 +1,38 @@
+﻿namespace ExceptionHandling.Enums
+{
+    /// <summary>
+    /// List of Tasks
+    /// </summary>
+    internal enum TaskList
+    {
+        /// <summary>
+        /// Task 1
+        /// </summary>
+        DivisionByZero = 1,
+
+        /// <summary>
+        /// Task 2
+        /// </summary>
+        IndexOutOfBound,
+
+        /// <summary>
+        /// Task 3
+        /// </summary>
+        CustomException,
+
+        /// <summary>
+        /// Task 4
+        /// </summary>
+        GlobalUnhandledException,
+
+        /// <summary>
+        /// Task 5
+        /// </summary>
+        ExceptionStackTrace,
+
+        /// <summary>
+        /// Exit
+        /// </summary>
+        Exit,
+    }
+}
